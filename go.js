a771b34b605a23e38f4cc404b11b440e62085649
Peter Hmíra <peter.hmira@gmail.com>
2020-04-10 04:49:31 +0200

    added files

--- go.js
+++ go.js
@@ -9,6 +9,7 @@ const STATE_WALKING = 1;
 const STATE_JUMP_START = 2;
 const STATE_JUMP_END = 3;
 const STATE_JUMPING = 4;
+const STATE_WRENCHES = 5;
 
 resize(app);
 // Add event listener so that our resize function runs every time the
@@ -27,7 +28,7 @@ document.body.appendChild(app.view);
 
 let animatedCapguy, walkingGuy, jumpGuyStart, jumpGuyEnd, background, background2, spritesheetname;
 
-let suhajWalk, suhajJump;
+let suhajWalk, suhajJump, suhajWrenches;
 
 let explosion;
 
@@ -40,12 +41,46 @@ let suhajState = STATE_WALKING;
 
 spritesheetname = usePng8 ? "images/spritesheet-png8.json" : "images/spritesheet.json";
 
+const suhajWrenchesFrames = [
+"assets/suhaj_wrenches_hq/suhaj_wrenches_000.png",
+"assets/suhaj_wrenches_hq/suhaj_wrenches_001.png",
+"assets/suhaj_wrenches_hq/suhaj_wrenches_002.png",
+"assets/suhaj_wrenches_hq/suhaj_wrenches_003.png",
+"assets/suhaj_wrenches_hq/suhaj_wrenches_004.png",
+"assets/suhaj_wrenches_hq/suhaj_wrenches_005.png",
+"assets/suhaj_wrenches_hq/suhaj_wrenches_006.png",
+"assets/suhaj_wrenches_hq/suhaj_wrenches_007.png",
+"assets/suhaj_wrenches_hq/suhaj_wrenches_008.png",
+"assets/suhaj_wrenches_hq/suhaj_wrenches_009.png",
+"assets/suhaj_wrenches_hq/suhaj_wrenches_010.png",
+"assets/suhaj_wrenches_hq/suhaj_wrenches_011.png",
+"assets/suhaj_wrenches_hq/suhaj_wrenches_012.png",
+"assets/suhaj_wrenches_hq/suhaj_wrenches_013.png",
+"assets/suhaj_wrenches_hq/suhaj_wrenches_014.png",
+"assets/suhaj_wrenches_hq/suhaj_wrenches_015.png",
+"assets/suhaj_wrenches_hq/suhaj_wrenches_016.png",
+"assets/suhaj_wrenches_hq/suhaj_wrenches_017.png",
+"assets/suhaj_wrenches_hq/suhaj_wrenches_018.png",
+"assets/suhaj_wrenches_hq/suhaj_wrenches_019.png",
+"assets/suhaj_wrenches_hq/suhaj_wrenches_020.png",
+"assets/suhaj_wrenches_hq/suhaj_wrenches_021.png",
+"assets/suhaj_wrenches_hq/suhaj_wrenches_022.png",
+"assets/suhaj_wrenches_hq/suhaj_wrenches_023.png",
+"assets/suhaj_wrenches_hq/suhaj_wrenches_024.png",
+"assets/suhaj_wrenches_hq/suhaj_wrenches_025.png",
+"assets/suhaj_wrenches_hq/suhaj_wrenches_026.png",
+"assets/suhaj_wrenches_hq/suhaj_wrenches_027.png",
+"assets/suhaj_wrenches_hq/suhaj_wrenches_028.png",
+"assets/suhaj_wrenches_hq/suhaj_wrenches_029.png"];
+
 // load sprite sheet image + data file, call setup() if completed
 PIXI.Loader.shared
     .add(spritesheetname)
     .add("assets/suhaj_walk.json")
     .add("assets/suhaj_jump.json")
+    .add("assets/suhaj_wrenches.json")
     .add("assets/explosion.json")
+    .add(suhajWrenchesFrames)
     .load(setup);
 
 
@@ -55,6 +90,7 @@ function setup() {
 
     let suhajWalkSheet = PIXI.Loader.shared.resources["assets/suhaj_walk.json"].spritesheet;
     let suhajJumpSheet = PIXI.Loader.shared.resources["assets/suhaj_jump.json"].spritesheet;
+    let suhajWrenchesSheet = PIXI.Loader.shared.resources["assets/suhaj_wrenches.json"].spritesheet;
 
     let explosionSheet = PIXI.Loader.shared.resources["assets/explosion.json"].spritesheet;
 
@@ -76,15 +112,32 @@ function setup() {
     suhajWalk.play();
 
     suhajJump = new PIXI.AnimatedSprite(suhajJumpSheet.animations["suhaj_jump"]);
-    suhajJump.position.set(400, background.height - 395);
+    suhajJump.position.set(400, background.height - 390);
     suhajJump.animationSpeed = 1;
     suhajJump.renderable = false;
     suhajJump.scale.x = 2;
     suhajJump.scale.y = 2;
     suhajJump.loop = false;
+    suhajJump.stop();
     suhajJump.onComplete = function() {
         suhajState = STATE_WALKING;
         updateSpritesByState();
+        console.log("jump ends");
+    };
+
+//    suhajWrenches = new PIXI.AnimatedSprite(suhajWrenchesSheet.animations["suhaj_wrenches"]);
+    suhajWrenches = PIXI.AnimatedSprite.fromFrames(suhajWrenchesFrames);
+    suhajWrenches.anchor.set(0.5)
+    suhajWrenches.position.set(400, background.height - 390);
+    suhajWrenches.animationSpeed = 0.5;
+    suhajWrenches.renderable = false;
+    suhajWrenches.scale.x = 2;
+    suhajWrenches.scale.y = 2;
+    suhajWrenches.loop = false;
+    suhajWrenches.onComplete = function() {
+        suhajState = STATE_WALKING;
+        updateSpritesByState();
+        console.log("hit ends");
     };
 
     explosion = new PIXI.AnimatedSprite(explosionSheet.animations["Explosion_Sequence_A "]);
@@ -93,6 +146,7 @@ function setup() {
 
     // add it to the stage and render!
     app.stage.addChild(suhajWalk);
+    app.stage.addChild(suhajWrenches);
     app.stage.addChild(suhajJump);
     app.stage.addChild(explosion);
     app.ticker.add(delta => gameLoop(delta));
@@ -102,6 +156,7 @@ function gameLoop(delta) {
     if (suhajWalk.x < background.width / 2) {
         suhajWalk.x = (suhajWalk.x + 3*delta) % (background.width + 200);
         suhajJump.x = suhajWalk.x;
+        suhajWrenches.x = suhajWalk.x;
     }
 
     //experimental game
@@ -121,6 +176,12 @@ function gameLoop(delta) {
     background2.x -= 1286;
 }
 
+function wrenchesHit() {
+    suhajState = STATE_WRENCHES;
+    updateSpritesByState();
+    console.log("hit starts");
+}
+
 function jump() {
     if (suhajState != STATE_WALKING) {
         return;
@@ -133,13 +194,18 @@ app.renderer.plugins.interaction.on('pointerdown', jump);
 function updateSpritesByState() {
     if (suhajState == STATE_WALKING) {
         suhajWalk.renderable = true;
-        suhajJump.gotoAndPlay(0);
+        suhajWalk.gotoAndPlay(0);
         suhajJump.renderable = false;
+        suhajWrenches.renderable = false;
     } else if (suhajState == STATE_JUMPING) {
         suhajJump.renderable = true;
         suhajJump.gotoAndPlay(0);
         suhajWalk.renderable = false;
-    } 
+    } else if (suhajState == STATE_WRENCHES) {
+        suhajWrenches.renderable = true;
+        suhajWrenches.gotoAndPlay(0);
+        suhajWalk.renderable = false;
+    }
 }
 
 function resize (app) {
