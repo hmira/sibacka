e0a919c7565afb7448c64071a8276d01f0a12963
Peter Hmíra <peter.hmira@gmail.com>
2020-04-10 02:46:36 +0200

    syn

--- go.js
+++ go.js
@@ -8,6 +8,7 @@ const WIDTH = 1920;
 const STATE_WALKING = 1;
 const STATE_JUMP_START = 2;
 const STATE_JUMP_END = 3;
+const STATE_JUMPING = 4;
 
 resize(app);
 // Add event listener so that our resize function runs every time the
@@ -26,7 +27,7 @@ document.body.appendChild(app.view);
 
 let animatedCapguy, walkingGuy, jumpGuyStart, jumpGuyEnd, background, background2, spritesheetname;
 
-let suhajWalk;
+let suhajWalk, suhajJump;
 
 let explosion;
 
@@ -46,6 +47,7 @@ PIXI.Loader.shared
     .add("assets/jump_start.json")
     .add("assets/jump_end.json")
     .add("assets/suhaj_walk.json")
+    .add("assets/suhaj_jump.json")
     .add("assets/explosion.json")
     .load(setup);
 
@@ -53,11 +55,10 @@ PIXI.Loader.shared
 function setup() {
     // the sprite sheet we've just loaded:
     let sheet = PIXI.Loader.shared.resources[spritesheetname].spritesheet;
-    let sheet2 = PIXI.Loader.shared.resources["assets/walking.json"].spritesheet;
-    let sheet3 = PIXI.Loader.shared.resources["assets/jump_start.json"].spritesheet;
-    let sheet4 = PIXI.Loader.shared.resources["assets/jump_end.json"].spritesheet;
 
     let suhajWalkSheet = PIXI.Loader.shared.resources["assets/suhaj_walk.json"].spritesheet;
+    let suhajJumpSheet = PIXI.Loader.shared.resources["assets/suhaj_jump.json"].spritesheet;
+
     let explosionSheet = PIXI.Loader.shared.resources["assets/explosion.json"].spritesheet;
 
     // initialize background sprite
@@ -69,74 +70,42 @@ function setup() {
     app.stage.scale.x = app.view.width / background.width;
     app.stage.scale.y = app.view.height / background.height;
 
-    // create an animated sprite
-    animatedCapguy = new PIXI.AnimatedSprite(sheet.animations["capguy"]);
-
-    // configure + start animation:
-    animatedCapguy.animationSpeed = 0.167;                  // 6 fps
-    animatedCapguy.position.set(0, background.height - 50); // almost bottom-left corner of the canvas
-    animatedCapguy.play();
-
-    walkingGuy = new PIXI.AnimatedSprite(sheet2.animations["walk"]);
-    walkingGuy.position.set(150, background.height - 180);
-    walkingGuy.animationSpeed = 1;
-    walkingGuy.play();
-
-    jumpGuyStart = new PIXI.AnimatedSprite(sheet3.animations["jump_start"]);
-    jumpGuyStart.position.set(550, background.height - 180);
-    jumpGuyStart.animationSpeed = 1;
-    jumpGuyStart.renderable = false;
-    jumpGuyStart.loop = false;
-
-    jumpGuyStart.onComplete = function() {
-        guyState = STATE_JUMP_END;
-        updateSpritesByState();
-    };
-
-    // jumpGuyStart.play();
-
-    jumpGuyEnd = new PIXI.AnimatedSprite(sheet4.animations["jump_end"]);
-    jumpGuyEnd.position.set(350, background.height - 180);
-    jumpGuyEnd.animationSpeed = 1;
-    jumpGuyEnd.renderable = false ;
-    jumpGuyEnd.loop = false;
-
-    jumpGuyEnd.onComplete = function() {
-        guyState = STATE_WALKING;
-        updateSpritesByState();
-    };
-
     suhajWalk = new PIXI.AnimatedSprite(suhajWalkSheet.animations["suhaj_walk"]);
-    suhajWalk.position.set(400, background.height - 300);
+    suhajWalk.position.set(400, background.height - 350);
     suhajWalk.animationSpeed = 1;
     suhajWalk.renderable = true;
-    suhajWalk.scale.x = 1.5;
-    suhajWalk.scale.y = 1.5;
+    suhajWalk.scale.x = 2;
+    suhajWalk.scale.y = 2;
     suhajWalk.play();
+
+    suhajJump = new PIXI.AnimatedSprite(suhajJumpSheet.animations["suhaj_jump"]);
+    suhajJump.position.set(400, background.height - 395);
+    suhajJump.animationSpeed = 1;
+    suhajJump.renderable = false;
+    suhajJump.scale.x = 2;
+    suhajJump.scale.y = 2;
+    suhajJump.loop = false;
+    suhajJump.onComplete = function() {
+        suhajState = STATE_WALKING;
+        updateSpritesByState();
+    };
 
     explosion = new PIXI.AnimatedSprite(explosionSheet.animations["Explosion_Sequence_A "]);
     explosion.position.set(300, 300);
     explosion.play();
 
     // add it to the stage and render!
-    app.stage.addChild(animatedCapguy);
-    app.stage.addChild(walkingGuy);
-    app.stage.addChild(jumpGuyStart);
-    app.stage.addChild(jumpGuyEnd);
     app.stage.addChild(suhajWalk);
+    app.stage.addChild(suhajJump);
     app.stage.addChild(explosion);
     app.ticker.add(delta => gameLoop(delta));
 }
 
 function gameLoop(delta) {
     if (suhajWalk.x < background.width / 2) {
-        suhajWalk.x = (suhajWalk.x + 5*delta) % (background.width + 200);
+        suhajWalk.x = (suhajWalk.x + 3*delta) % (background.width + 200);
+        suhajJump.x = suhajWalk.x;
     }
-
-    animatedCapguy.x = (animatedCapguy.x + 5*delta) % (background.width + 200);
-    walkingGuy.x = (walkingGuy.x + 5*delta) % (background.width + 200);
-    jumpGuyStart.x = walkingGuy.x;
-    jumpGuyEnd.x = walkingGuy.x;
 
     //experimental game
     postition += 10;
@@ -156,30 +125,24 @@ function gameLoop(delta) {
 }
 
 function jump() {
-    if (guyState != STATE_WALKING) {
+    if (suhajState != STATE_WALKING) {
         return;
     }
-    guyState = STATE_JUMP_START;
+    suhajState = STATE_JUMPING;
     updateSpritesByState();
 }
 app.renderer.plugins.interaction.on('pointerdown', jump);
 
 function updateSpritesByState() {
-    if (guyState == STATE_WALKING) {
-        walkingGuy.renderable = true;
-        jumpGuyStart.renderable = false;
-        jumpGuyEnd.renderable = false;
-    } else if (guyState == STATE_JUMP_START) {
-        walkingGuy.renderable = false;
-        jumpGuyStart.renderable = true;
-        jumpGuyStart.gotoAndPlay(0);
-        jumpGuyEnd.renderable = false;
-    } else if (guyState == STATE_JUMP_END) {
-        walkingGuy.renderable = false;
-        jumpGuyStart.renderable = false;
-        jumpGuyEnd.renderable = true;
-        jumpGuyEnd.gotoAndPlay(0);
-    }
+    if (suhajState == STATE_WALKING) {
+        suhajWalk.renderable = true;
+        suhajJump.gotoAndPlay(0);
+        suhajJump.renderable = false;
+    } else if (suhajState == STATE_JUMPING) {
+        suhajJump.renderable = true;
+        suhajJump.gotoAndPlay(0);
+        suhajWalk.renderable = false;
+    } 
 }
 
 function resize (app) {
