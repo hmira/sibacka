7e0cda5628c86f84446e8302bb8172d380321b7b
Peter Hmíra <peter.hmira@gmail.com>
2020-04-13 06:09:30 +0200

    added base of gameplay

--- go.js
+++ go.js
@@ -16,7 +16,9 @@ window.addEventListener("resize", resize(app));
 
 document.body.appendChild(app.view);
 
-let animatedCapguy, walkingGuy, jumpGuyStart, jumpGuyEnd, background, background2, wrenches;
+let animatedCapguy, walkingGuy, jumpGuyStart, jumpGuyEnd, background, background2, wrenches, bush1;
+
+let bronzeEgg, silverEgg, goldEgg;
 
 let topBackgroundOffset;
 
@@ -28,8 +30,12 @@ let postition = 0;
 
 let suhajState = STATE_WALKING;
 
+let score = 0;
+let level = 1;
+
 const suhajYoffset = 100;
 const wrenchesYOffset = 178;
+const bushOffset = 198;
 
 // spritesheetname = usePng8 ? "images/spritesheet-png8.json" : "images/spritesheet.json";
 
@@ -135,23 +141,62 @@ function setup() {
     suhajWrenches.onComplete = function() {
         suhajState = STATE_WALKING;
         updateSpritesByState();
-        console.log("hit ends");
     };
 
+    bronzeEgg = new PIXI.Sprite(sheet.textures["bronze_egg.png"]);
+    bronzeEgg.anchor.set(0.5);
+    bronzeEgg.scale.set(0.5);
+    bronzeEgg.position.set(background.width - 100, 100);
+    bronzeEgg.renderable = false;
+
+    silverEgg = new PIXI.Sprite(sheet.textures["silver_egg.png"]);
+    silverEgg.anchor.set(0.5);
+    silverEgg.scale.set(0.5);
+    silverEgg.position.set(background.width - 200, 100);
+    silverEgg.renderable = false;
+    
+    goldEgg = new PIXI.Sprite(sheet.textures["gold_egg.png"]);
+    goldEgg.anchor.set(0.5);
+    goldEgg.scale.set(0.5);
+    goldEgg.position.set(background.width - 300, 100);
+    goldEgg.renderable = false;
+
     explosion = new PIXI.AnimatedSprite(explosionSheet.animations["Explosion_Sequence_A "]);
-    explosion.position.set(300, 300);
-    explosion.play();
+    explosion.position.set(background.width - 100, 100);
+    explosion.stop();
+    explosion.loop = false;
+    explosion.renderable = false;
+    explosion.onComplete = function() {
+        if (level == 2) {
+            bronzeEgg.renderable = true;
+        }
+        if (level == 3) {
+            silverEgg.renderable = true;
+        }
+        if (level == 4) {
+            goldEgg.renderable = true;
+        }
+        explosion.renderable = false;
+    };
 
     wrenches = new PIXI.Sprite(sheet.textures["rake.png"]);
     wrenches.scale.set(1.5);
     wrenches.position.set(WIDTH + 1, background.height - wrenchesYOffset);
+
+    bush1 = new PIXI.Sprite(sheet.textures["bush.png"]);
+    bush1.position.set(WIDTH + 1, background.height - wrenchesYOffset);
+    bush1.scale.set(2);
 
     // add it to the stage and render!
     app.stage.addChild(wrenches);
     app.stage.addChild(suhajWalk);
     app.stage.addChild(suhajWrenches);
     app.stage.addChild(suhajJump);
+    app.stage.addChild(bronzeEgg);
+    app.stage.addChild(silverEgg);
+    app.stage.addChild(goldEgg);
     app.stage.addChild(explosion);
+    app.stage.addChild(bush1);
     app.ticker.add(delta => gameLoop(delta));
 
     resize(app).call();
@@ -163,6 +208,7 @@ function updateYs() {
     suhajWrenches.y = background.height - suhajYoffset + topBackgroundOffset;
     suhajJump.y = background.height - suhajYoffset + topBackgroundOffset;
     wrenches.y = background.height - wrenchesYOffset + topBackgroundOffset;
+    bush1.y = background.height - bushOffset + topBackgroundOffset;
 }
 
 function gameLoop(delta) {
@@ -189,15 +235,45 @@ function gameLoop(delta) {
     background2.x -= WIDTH;
 
     wrenches.x -= 12;
+    bush1.x -= 10;
+
+    if (bush1.x < -360) {
+        bush1.x = WIDTH + 1;
+    }
+
     if (wrenches.x < -460) {
         wrenches.x = WIDTH + 1;
+        wrenches.renderable = true;
+
+        score += 1;
+
+        if (score == 10 ) {
+            level = 2;
+            explosion.x = bronzeEgg.x;
+            explosion.renderable = true;
+            explosion.gotoAndPlay(0);
+        } else if (score == 20 ) {
+            level = 3;
+            explosion.x = silverEgg.x;
+            explosion.renderable = true;
+            explosion.gotoAndPlay(0);
+        } else if (score == 30) {
+            level = 4;
+            explosion.x = goldEgg.x;
+            explosion.renderable = true;
+            explosion.gotoAndPlay(0);
+        }
+    }
+
+    if (wrenches.x > 450 && wrenches.x < 700 && suhajState == STATE_WALKING) {
+        wrenchesHit();
+        wrenches.renderable = false;
     }
 }
 
 function wrenchesHit() {
     suhajState = STATE_WRENCHES;
     updateSpritesByState();
-    console.log("hit starts");
 }
 
 function jump() {
